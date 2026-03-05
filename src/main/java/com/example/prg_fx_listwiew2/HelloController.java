731c5997aab77df538607440f85dbfb45240fe6c
drtluk00 <drtluk00@gasos-ro.cz>
2026-03-05 11:18:52 +0100

    trh

--- src/main/java/com/example/prg_fx_listwiew2/HelloController.java
+++ src/main/java/com/example/prg_fx_listwiew2/HelloController.java
@@ -1,11 +1,15 @@
 package com.example.prg_fx_listwiew2;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Label labelDetail;
     @FXML
     private TextField textFieldNazevUkolu;
 
@@ -20,9 +24,35 @@ public class HelloController {
     @FXML
     private CheckBox splneno;
 
+    @FXML
+    private Button buttonPridat;
+    @FXML
+    private Button buttonVybrat;
+
+
+
+    @FXML
+    private ListView<Ukol> ukolListView;
+    public ObservableList<Ukol> ukolObservableList = FXCollections.observableArrayList();
+
 
     @FXML
     protected void onPridat() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        Ukol ukol = new Ukol(textFieldNazevUkolu.getText(), ((RadioButton) predmet.getSelectedToggle()).getText(), ((RadioButton) priorita.getSelectedToggle()).getText(), splneno.isSelected() );
+        ukolObservableList.add(ukol);
+        ukolListView.setItems(ukolObservableList);
+        predmet.getSelectedToggle().setSelected(false);
+        priorita.getSelectedToggle().setSelected(false);
+        splneno.setSelected(false);
+
     }
+
+    @FXML
+    protected void onVybrat() {
+        Ukol vybranyUkol = ukolListView.getSelectionModel().getSelectedItem();
+
+        String stav = vybranyUkol.isSplneno() ? "[SPLNĚNO]" : "[NESPLNĚNO]";
+        labelDetail.setText("Název: " +vybranyUkol.getJmeno()+ " Předmět: " +vybranyUkol.getPredmet()+ " Priorita: " +vybranyUkol.getPriorita()+ " Stav: " +stav );
+    }
+
 }
--- src/main/java/com/example/prg_fx_listwiew2/Ukol.java
+++ src/main/java/com/example/prg_fx_listwiew2/Ukol.java
@@ -7,5 +7,32 @@ private String predmet;
 private String priorita;
 private boolean splneno;
 
+public Ukol(String jmeno, String predmet, String priorita, boolean splneno) {
+    this.jmeno=jmeno;
+    this.predmet=predmet;
+    this.priorita=priorita;
+    this.splneno=splneno;
+}
 
+public String toString(){
+    String stav = splneno ? "[HOTOVO]" : "[ ]";
+    return jmeno+ " " +predmet+ " " +priorita+ " " +stav;
+}
+
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public String getPredmet() {
+        return predmet;
+    }
+
+    public String getPriorita() {
+        return priorita;
+    }
+
+    public boolean isSplneno() {
+        return splneno;
+    }
 }
